883ec0136977899d5fa27b6cc2d8eaece9fc7a5b
Yashasvi Tripathi <yashasvitripathi22@gmail.com>
2022-05-30 14:50:01 +0530

    first commit

---
+++ action.js
@@ -0,0 +1,8 @@
+// action for menuShow & menuHide Toggle Menu
+var navLink = document.getElementById("nav-link");
+function showMenu(){
+    navLink.style.right="0";
+}
+function hideMenu(){
+    navLink.style.right="-200px";
+}
